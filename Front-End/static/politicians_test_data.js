ff11f2f78ee37f78eb06d8b703b44683b1d6b8bb
Kevin Kuney <Kevin.Kuney@gmail.com>
2018-07-12 18:30:16 -0500

    Moved politicians test data into politicians_test_data.js

---
+++ Front-End/static/politicians_test_data.js
@@ -0,0 +1,47 @@
+// test data
+var data_barack = 	{
+			chamber : "house",
+			contact_form: null,
+			first_name: "Barack",
+			id: 1,
+			last_name: "Obama",
+			party: "D",
+			phone: "202-225-8490", 
+			site: "https://abraham.house.gov", 
+			state: "WA",
+			imgage_srouce : "https://bloximages.chicago2.vip.townnews.com/stljewishlight.com/content/tncms/assets/v3/editorial/b/03/b037b0de-4830-11e1-a8ed-001871e3ce6c/4f216e3f0426f.image.jpg?resize=558%2C759",
+			href : "/politicians/barack_obama"
+			};
+
+
+var data_cruz = 	{
+			chamber : "house",
+			contact_form: null,
+			first_name: "Ted",
+			id: 2,
+			last_name: "Cruz",
+			party: "R",
+			phone: "202-225-8490", 
+			site: "https://abraham.house.gov", 
+			state: "TX",
+			imgage_srouce : "https://upload.wikimedia.org/wikipedia/commons/8/87/Ted_Cruz%2C_official_portrait%2C_113th_Congress.jpg",
+			href : "/politicians/ted_cruz"
+			};
+
+var data_mccain = 	{
+
+			chamber : "Senate",
+			contact_form: null,
+			first_name: "John",
+			id: 2,
+			last_name: "McCain",
+			party: "R",
+			phone: "202-225-8490", 
+			site: "https://abraham.house.gov", 
+			state: "AZ",
+			imgage_srouce : "https://upload.wikimedia.org/wikipedia/commons/e/e1/John_McCain_official_portrait_2009.jpg",
+			href : "/politicians/john_mccain"
+			};
+
+var politician_array = [data_barack, data_cruz, data_mccain];
+var row_array = [politician_array,politician_array,politician_array];
