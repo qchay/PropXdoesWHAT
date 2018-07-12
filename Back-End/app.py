6c14c3b7b570cfa3ac885a06a527a4a9dde63317
Kevin Kuney <Kevin.Kuney@gmail.com>
2018-07-11 19:16:51 -0500

    Updated Back-End to accept new politician table

--- Back-End/app.py
+++ Back-End/app.py
@@ -1,23 +1,26 @@
+# pylint: disable-all
 from flask import Flask
-
 from flask_restless import APIManager
-
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Propxdoeswhat:Propxdoeswhat@propxdoeswhatdb.cnsfq0o1clx8.us-east-2.rds.amazonaws.com/PropxdoeswhatDB'
-
 db = SQLAlchemy(app)
 
 class politicians (db.Model):
-   first_name = db.Column(db.Unicode)
-   last_name = db.Column(db.Unicode)
-   state = db.Column(db.Unicode)
-   politician_id = db.Column(db.Integer, primary_key=True)
+	id = db.Column(db.Integer, primary_key=True, nullable=False)
+	first_name = db.Column(db.Unicode, nullable=False)
+	last_name = db.Column(db.Unicode, nullable=False)
+	chamber = db.Column(db.Unicode, nullable=False) 
+	state = db.Column(db.Unicode, nullable=False)
+	party = db.Column(db.Unicode, nullable=False)
+	site = db.Column(db.Unicode, nullable=False)
+	contact_form = db.Column(db.Unicode, nullable=False)
+	phone = db.Column(db.Unicode, nullable=False)
+	raw = db.Column(db.Unicode, nullable=False)
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
-
 manager.create_api(politicians, methods =['GET'])
 
-app.run(host ='0.0.0.0')
+app.run(host ='0.0.0.0', debug = True)
