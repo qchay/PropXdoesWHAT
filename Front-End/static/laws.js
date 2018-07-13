ce56ad75ca7fe62101a3260bb4d59bd7f522dbb5
Kevin Kuney <Kevin.Kuney@gmail.com>
2018-07-13 15:10:58 -0500

    Laws now draws from the database and supports pagination

--- Front-End/static/laws.js
+++ Front-End/static/laws.js
@@ -1,19 +1,16 @@
 class Row extends React.Component {
-	constructor(props) {
-		super(props);
-		}
+	constructor(props) { super(props); }
 	render() {
   		return (
  			<div className="row">
- 				<Laws data={this.props.dataArray[0]}/>
-				<Laws data={this.props.dataArray[1]}/>
-				<Laws data={this.props.dataArray[2]}/>
+ 				{this.props.law_array.map((law_array_item, i) => 
+ 					<Law key = {i} law_data = {law_array_item}/>)}
 			</div>
     	);
   	}
 }	
 
-class Laws extends React.Component {
+class Law extends React.Component {
 	constructor(props) {
 		super(props);
 		}
@@ -21,18 +18,18 @@ class Laws extends React.Component {
   		return (
 	  		<div className="col-md-4">
 	 			<div className="card mb-4 box-shadow" id="law-card">
-					<a href={this.props.data.href}>
+					<a href={'/politicians/' + this.props.law_data.name + '/' + this.props.law_data.id}>
 						<div id="law-headshot-box">
-							<img className="card-img-top" id="law-headshot" src={this.props.data.image_source} alt="Card image cap"/>
+							<img className="card-img-top" id="law-headshot" src="https://cdn1.edgedatg.com/aws/v2/abc/SchoolhouseRock/episode/1697098/212f1befce5deb621a01c48a6d717dea/1000x563-Q90_212f1befce5deb621a01c48a6d717dea.jpg" alt="Card image cap"/>
 						</div>
 					</a>
 
 					<div className="card-body">
-						<h3 id="card-name">{this.props.data.name}</h3>
+						<h3 id="card-name">{this.props.law_data.title.substring(0,50) + (this.props.law_data.title.length > 50 ? '...' : ' ')}</h3>
 						<div id="card-attr">
-							<li>Affected Groups: {this.props.data.affectedGroups}</li>
-							<li>Subject: {this.props.data.subject}</li>
-							<li>Established: {this.props.data.establishedDate}</li>
+							<li>Sponsor: {this.props.law_data.sponsor.first_name + ' ' + this.props.law_data.sponsor.last_name }</li>
+							<li>Subject: {this.props.law_data.subject}</li>
+							<li>Introduced: {this.props.law_data.introduced}</li>
 						</div>
 					</div>
 				</div>
@@ -41,40 +38,39 @@ class Laws extends React.Component {
   	}
 }	
 
-var data_1 = 	{
-			name : "National Invasive Species Act of 1996",
-			affectedGroups : "Veterans",
-			subject : "Environmental protection",
-			establishedDate : "1996-10-26",
-			image_source : "https://cdn1.edgedatg.com/aws/v2/abc/SchoolhouseRock/episode/1697098/212f1befce5deb621a01c48a6d717dea/1000x563-Q90_212f1befce5deb621a01c48a6d717dea.jpg",
-			href : "/laws/national_invasive_species_act_of_1996"
-			};
+// Getting json response
+var page_number = JSON.parse(document.getElementById("page_number").dataset.page);
+var httpRequest = new XMLHttpRequest();
+var api = "http://api.propxdoeswhat.me/api/laws?page=" + page_number;
+httpRequest.open("GET", api, false);
+httpRequest.send();
+var jsonResponse = JSON.parse(httpRequest.responseText);
 
-var data_2 = 	{
-			name : "Protect Our Kids Act of 2012",
-			affectedGroups : "Kids",
-			subject : "Kid protection",
-			establishedDate : "2012-12-19",
-			image_source : "https://cdn1.edgedatg.com/aws/v2/abc/SchoolhouseRock/episode/1697098/212f1befce5deb621a01c48a6d717dea/1000x563-Q90_212f1befce5deb621a01c48a6d717dea.jpg",
-			href : "/laws/protect_our_kids_act_of_2012"
-			};
+// Parsing json response, putting data into rows
+var row_array = [];
+var law_array = [];
+var count = 1;
+for (var law of jsonResponse.objects) {
+	if (!(count%3)){
+		law_array.push(law);
+		row_array.push(law_array);
+		var law_array = [];
+	} else {
+		law_array.push(law);
+	}
 
-var data_3 = 	{
-			name : "Dignified Burial and Other Veterans' Benefits Improvement Act of 2012",
-			affectedGroups : "Working Class",
-			subject : "Veterans",
-			establishedDate : "2012-12-30",
-			image_source : "https://cdn1.edgedatg.com/aws/v2/abc/SchoolhouseRock/episode/1697098/212f1befce5deb621a01c48a6d717dea/1000x563-Q90_212f1befce5deb621a01c48a6d717dea.jpg",
-			href : "/action_groups/dignified_burial_and_other_veterans_benefits_improvement_act_of_2012"
-			};
+	if ((count == jsonResponse.objects.length) && (count%3)) {
+		row_array.push(law_array);
+	}
+	count++;
+}
 
-var dataArray = [data_1, data_2, data_3];
-
+// Rendering DOM elements
 ReactDOM.render(
-	<div className="container">
-		<Row dataArray={dataArray}/>
-		<Row dataArray={dataArray}/>
-		<Row dataArray={dataArray}/>
+	<div className="container">	
+    	{row_array.map((row_array_item, i) => <Row key = {i} law_array = {row_array_item}/>)}
 	</div>, 
-	document.getElementById('target')
+	document.getElementById('album')
 );
+
+
