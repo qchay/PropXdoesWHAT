31c03f38d797c7497feb2b0581a42076eacebcd3
Kevin Kuney <Kevin.Kuney@gmail.com>
2018-07-20 18:06:29 -0500

    Added Political Bio Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 import Law_Page from "./components/Law_Page";
 // import Law_Bio from "./Law_Bio";
 import Politician_Page from "./components/Politician_Page";
-// import Politician_Bio from "./Politician_Bio";
+import Politician_Bio from "./components/Politician_Bio";
 import Action_Group_Page from "./components/Action_Group_Page";
 import About from "./components/About/About";
 
@@ -24,8 +24,8 @@ class App extends Component {
               <Route path='/laws/page/:page_number' component={Law_Page} />
               <Route path='/politicians/page/:page_number' component={Politician_Page} />
               <Route path='/action_groups/page/:page_number' component={Action_Group_Page} />
+              <Route path='/politicians/:politcian_name/:id' component={Politician_Bio} />
               <Route path='/about' component={About} />
-
             </Switch>
           </div>
         </Router>
