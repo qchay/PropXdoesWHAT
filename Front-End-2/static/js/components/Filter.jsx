93822432dda3329535f15bb7ccbe2de4e0571e94
Dustin Huang <dustinhuang21@gmail.com>
2018-07-19 19:35:24 -0500

    Filter

--- Front-End-2/static/js/components/Filter.jsx
+++ Front-End-2/static/js/components/Filter.jsx
@@ -1,28 +1,42 @@
-// Row.jsx
-import React from 'react';
+import React from 'react'; 
+import Select from 'react-select'; 
+import makeAnimated from 'react-select/lib/animated';
 
-export default class Filter extends React.Component {
-	constructor(props) { 
-		super(props); 
-		this.state = {buttonclicked: "Nope"};
-		this.filterbutton = this.filterbutton.bind(this);
-	}
+export default class Filter extends React.Component {  
+	constructor(props) {     
+		super(props);     
+		this.onChange = this.onChange.bind(this);     
+		this.state = { value: [] }    
+	}  
 
-	filterbutton () {
-		console.log("filterbutton")
-		this.setState({buttonclicked :"Yep"});
-		let filterjson = {"filters":[{"name":"first_name","op":"eq","val":"Ted"},{"name":"last_name","op":"eq","val":"Cruz"}]}
-		console.log(this.props.getJsonResponseCallBack);
-		this.props.getJsonResponseCallBack(filterjson);
-	}
-	render() {
-  		return (
-  			<div>
-	  			<p> {this.state.buttonclicked} </p>
-	  			<button onClick={this.filterbutton}>
-	  				Filter
-				</button>
-			</div>
-    	);
-  	}
-}	
+	onChange(val) {     
+		var string = "{\"filters\":[{\"or\":["     
+		var i;     
+		for(i = 0; i <val.length;i++){       
+			if(i!=0){         
+				string+=",";       
+			}       
+			string+="{\"name\":\""+this.props.type+"\",\"op\":\"eq\",\"val\":\""+val[i].value+"\"}"     
+		}     
+		string+="]}]}"     
+		console.log(string);      
+		let filterjson = JSON.parse(string)     
+		this.props.getJsonResponseCallBack(filterjson);     
+		this.setState({value: val})    
+	}   
+
+	render(){   
+		return (     
+			<div>     
+			<Select       
+			closeMenuOnSelect={false}       
+			components={makeAnimated()}       
+			onChange={this.onChange}       
+			isMulti       
+			options={this.props.filterOptions}       
+			value={this.state.value}      
+			/>     
+			</div>   
+			); 
+	} 
+}
--- Front-End-2/static/js/components/Politician_Page.jsx
+++ Front-End-2/static/js/components/Politician_Page.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import Album from './Album';
 import Filter from './Filter';
-import PartyFilter from './PartyFilter';
 import Page_Footer from './Page_Footer'
 
 export default class Politician_Page extends React.Component {
@@ -53,31 +52,31 @@ export default class Politician_Page extends React.Component {
 	}
 
 	getPageData(jsonResponse) {
-	const page = jsonResponse.page;
-	const total_pages = jsonResponse.total_pages;
-	var page_count = page - 2;
-	var page_array = [];
-	if ((total_pages - page) < 2) {
-		page_count += -2 + total_pages - page;
-	}
-	while (true) {
-		while (page_count < 1) { 
+		const page = jsonResponse.page;
+		const total_pages = jsonResponse.total_pages;
+		var page_count = page - 2;
+		var page_array = [];
+		if ((total_pages - page) < 2) {
+			page_count += -2 + total_pages - page;
+		}
+		while (true) {
+			while (page_count < 1) { 
+				page_count++;
+			}
+			page_array.push(page_count);
+			if ((page_count === total_pages) || (page_array.length === 5)) {
+				break;
+			}
 			page_count++;
 		}
-		page_array.push(page_count);
-		if ((page_count === total_pages) || (page_array.length === 5)) {
-			break;
-		}
-		page_count++;
-	}
-	var page_data = {
-		page: page, 
-		total_pages: total_pages, 
-		page_array: page_array, 
-		prev_page: (page === 1 ? 1 : page - 1),
-		next_page: (page === total_pages ? total_pages : page + 1)
-		}
-	return page_data;
+		var page_data = {
+			page: page, 
+			total_pages: total_pages, 
+			page_array: page_array, 
+			prev_page: (page === 1 ? 1 : page - 1),
+			next_page: (page === total_pages ? total_pages : page + 1)
+			}
+		return page_data;
 	}
 
 	render() {
@@ -88,6 +87,58 @@ export default class Politician_Page extends React.Component {
 		  { value: 'I', label: 'Independent' }
 		];
 
+		const stateOptions = [
+		{ label: 'AL' , value: 'AL'},
+		{ label: 'AK' , value: 'AK'},
+		{ label: 'AZ' , value: 'AZ'},
+		{ label: 'AR' , value: 'AR'},
+		{ label: 'CA' , value: 'CA'},
+		{ label: 'CO' , value: 'CO'},
+		{ label: 'CT' , value: 'CT'},
+		{ label: 'DE' , value: 'DE'},
+		{ label: 'FL' , value: 'FL'},
+		{ label: 'GA' , value: 'GA'},
+		{ label: 'HI' , value: 'HI'},
+		{ label: 'ID' , value: 'ID'},
+		{ label: 'IL' , value: 'IL'},
+		{ label: 'IN' , value: 'IN'},
+		{ label: 'IA' , value: 'IA'},
+		{ label: 'KS' , value: 'KS'},
+		{ label: 'KY' , value: 'KY'},
+		{ label: 'LA' , value: 'LA'},
+		{ label: 'ME' , value: 'ME'},
+		{ label: 'MD' , value: 'MD'},
+		{ label: 'MA' , value: 'MA'},
+		{ label: 'MI' , value: 'MI'},
+		{ label: 'MN' , value: 'MN'},
+		{ label: 'MS' , value: 'MS'},
+		{ label: 'MO' , value: 'MO'},
+		{ label: 'MT' , value: 'MT'},
+		{ label: 'NE' , value: 'NE'},
+		{ label: 'NV' , value: 'NV'},
+		{ label: 'NH' , value: 'NH'},
+		{ label: 'NJ' , value: 'NJ'},
+		{ label: 'NM' , value: 'NM'},
+		{ label: 'NY' , value: 'NY'},
+		{ label: 'NC' , value: 'NC'},
+		{ label: 'ND' , value: 'ND'},
+		{ label: 'OH' , value: 'OH'},
+		{ label: 'OK' , value: 'OK'},
+		{ label: 'OR' , value: 'OR'},
+		{ label: 'PA' , value: 'PA'},
+		{ label: 'RI' , value: 'RI'},
+		{ label: 'SC' , value: 'SC'},
+		{ label: 'SD' , value: 'SD'},
+		{ label: 'TN' , value: 'TN'},
+		{ label: 'TX' , value: 'TX'},
+		{ label: 'UT' , value: 'UT'},
+		{ label: 'VT' , value: 'VT'},
+		{ label: 'VA' , value: 'VA'},
+		{ label: 'WA' , value: 'WA'},
+		{ label: 'WV' , value: 'WV'},
+		{ label: 'WI' , value: 'WI'},
+		{ label: 'WY' , value: 'WY'}]
+
 		let page_data = this.getPageData(this.state.jsonResponse);
 		console.log(page_data);
 		let page_name = "politicians"
@@ -95,7 +146,9 @@ export default class Politician_Page extends React.Component {
   			<div>
 				<p>Politician_Page! Page: {this.props.match.params.page_number}</p>
 				<main>
-					<PartyFilter getJsonResponseCallBack = {this.getJsonResponseCallBack} filterOptions={partyOptions}/>
+					<Filter getJsonResponseCallBack = {this.getJsonResponseCallBack} filterOptions={partyOptions} type={"party"}/>
+					<Filter getJsonResponseCallBack = {this.getJsonResponseCallBack} filterOptions={stateOptions} type={"state"}/>
+
 					<Album row_array={row_array} page_name={"politician_page"} />
 					<Page_Footer page_data={page_data} page_name={page_name}/>
 				</main>
