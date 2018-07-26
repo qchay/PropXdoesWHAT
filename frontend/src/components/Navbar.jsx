53b2284016bcee2572c6f03493e0dd7f25ac2eee
jaelee0409 <jaelee0409@gmail.com>
2018-07-26 13:55:01 -0500

    Navbar for visualizations

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,6 +25,20 @@ export default class Navbar extends React.Component {
 						<li className="nav-item">
 							<a className="nav-link" href="/affected_groups/page/1">Affected Groups</a>
 						</li>
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								Visualizations
+							</a>
+							<div className="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a className="dropdown-item" href="/visualizations/politicians">Politicians</a>
+								<a className="dropdown-item" href="/visualizations/laws">Laws</a>
+								<a className="dropdown-item" href="#">3</a>
+								<div className="dropdown-divider"></div>
+								<a className="dropdown-item" href="#">4</a>
+								<a className="dropdown-item" href="#">5</a>
+								<a className="dropdown-item" href="#">6</a>
+							</div>
+						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="/about">About</a>
 						</li>
