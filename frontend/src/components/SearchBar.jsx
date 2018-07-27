0cb1ad471fe4f66d0d17f35139c2368ec6172b38
Kevin Kuney <Kevin.Kuney@gmail.com>
2018-07-26 19:57:42 -0500

    Search All page now functions

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,20 +3,18 @@ import React from 'react';
 export default class Search extends React.Component {
     constructor(props) {
     super(props);
-    this.state = {inputValue : this.props.defaultValue}
+    this.state = {inputValue : ""}
     this.handleClick = this.handleClick.bind(this);
     this.updateInputValue = this.updateInputValue.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   handleClick() {
-    console.log("*** handleClick in Search ***");
     let update = {name: "search", value : this.state.inputValue};
     this.props.searchCallBack([update])
   }
 
   handleKeyPress(event){
-    console.log("*** handleKeyPress in Search ***")
     if(event.key === 'Enter'){
       this.setState({inputValue: event.target.value});
       this.handleClick();
@@ -24,16 +22,15 @@ export default class Search extends React.Component {
   }
 
   updateInputValue(event) {
-    console.log("*** updateInputValue in Search ***")
     this.setState({inputValue: event.target.value});
   }
 
   render() {
     return (
      <div>
-        <form className="form-inline my-2 my-lg-0" action="/search/">
+        <form className="form-inline my-2 my-lg-0" action="/search">
 			<input 
-			name="q"
+			name="search"
 			className="form-control mr-sm-2" 
 			type="search" 
 			placeholder="Search" 
